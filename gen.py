23979fc5868e47679034c8c921c41ff8c8651c61
shijianan201 <xiaojiangagaga@gmail.com>
2022-06-28 17:08:07 +0800

    跳针

--- gen.py
+++ gen.py
@@ -1,6 +1,8 @@
 import qrcode
 # 生成二维码
-img = qrcode.make(data="你好")
+byt = open("qr_code.txt").readline()
+print(byt)
+img = qrcode.make(data=byt)
 # 将二维码保存为图片
 with open('test.png', 'wb') as f:
     img.save(f)
